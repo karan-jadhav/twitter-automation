c61fc023c597ce03478f490456457eabdee6efda
iamkj <karan.jadhavhkr@gmail.com>
2020-01-12 20:57:29 +0530

    calback view added

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,5 @@ from django.urls import  path
 from . import views as view
 urlpatterns = [
     path('', view.index, name='home-page'),
+    path('callback/', view.callback, name='callback'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import  HttpResponse
 import tweepy
 
 # Create your views here.
 
 def index(request):
     return render(request, 'home/index.html')
+
+def callback(request):
+    return HttpResponse('200 ok')
